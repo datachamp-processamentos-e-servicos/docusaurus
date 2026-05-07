b7593671daa8ad2048f8ec90c4c214ee2f4ff1da
Nicolau Foppa <nicolau@datachamp.com.br>
2026-05-07 09:17:01 -0300

    DONE - teste build para o github pages

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/datachamp/projects/docusaurus/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/datachamp/projects/docusaurus/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/datachamp/projects/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/datachamp/projects/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/datachamp/projects/docusaurus/src/css/custom.css"),
+];
--- build/assets/js/c15d9823.46bc6f98.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkteste=self.webpackChunkteste||[]).push([["6431"],{4566(e){e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
