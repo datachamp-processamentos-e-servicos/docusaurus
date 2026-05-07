595ed58de33d841815e40d075bc88db5a378a0ac
Augusto Foppa <augusto@datachamp.com.br>
2026-05-07 16:15:45 -0300

    Modificado trailingSlash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,9 +31,8 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://datachamp-processamentos-e-servicos.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'docusaurus/',
+  baseUrl: '/docusaurus/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
